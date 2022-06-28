d39bfff4ad1b16cafa1c248694ab86ee77f8322b
Yuts-T34 <68275906+Yuts-T34@users.noreply.github.com>
2022-06-28 19:14:04 +0700

    Add files via upload

---
+++ order.js
@@ -0,0 +1,37 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNunmer: '8858',
+        paymentStatus: 'Refunded',
+        shipping: 'Pennding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNunmer: '8858',
+        paymentStatus: 'Refunded',
+        shipping: 'Pennding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNunmer: '8858',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNunmer: '8858',
+        paymentStatus: 'Refunded',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNunmer: '8858',
+        paymentStatus: 'Refunded',
+        shipping: 'Pennding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNunmer: '8858',
+        paymentStatus: 'Refunded',
+        shipping: 'Delivered'
+    }
+]
